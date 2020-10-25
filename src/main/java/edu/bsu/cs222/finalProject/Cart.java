f8e034236fdef64d0d84ff9630cb99b67af6cdcc
Caleb Van Lue <cpvanlue@bsu.edu>
2020-10-25 18:39:04 -0400

    Added methods to add and remove items from a cart.

--- src/main/java/edu/bsu/cs222/finalProject/Cart.java
+++ src/main/java/edu/bsu/cs222/finalProject/Cart.java
@@ -1,4 +1,24 @@
 package edu.bsu.cs222.finalProject;
 
+import java.util.ArrayList;
+
 public class Cart {
+
+    private ArrayList<Item> cartItems;
+
+    public Cart(ArrayList<Item> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public ArrayList<Item> getCartItems() {
+        return cartItems;
+    }
+
+    public void add(Item itemToCart) {
+        cartItems.add(itemToCart);
+    }
+
+    public void remove(Item itemToCart) {
+        cartItems.remove(itemToCart);
+    }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,4 +1,35 @@
+import edu.bsu.cs222.finalProject.Cart;
+import edu.bsu.cs222.finalProject.Inventory;
+import edu.bsu.cs222.finalProject.Item;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static edu.bsu.cs222.finalProject.Inventory.collectItemsFromResources;
+import static edu.bsu.cs222.finalProject.Inventory.createArrayListOfItems;
 
 public class CartTest {
+
+    @Test
+    public void testCanAddItemToCart() throws IOException {
+        Cart cart = new Cart(new ArrayList<>());
+        Inventory inventory = new Inventory(createArrayListOfItems(collectItemsFromResources()));
+        Item itemToCart = inventory.getItems().get(0);
+        cart.add(itemToCart);
+        Assertions.assertEquals("\"Apple\"", cart.getCartItems().get(0).getName());
+        Assertions.assertEquals("1.99", cart.getCartItems().get(0).getPrice());
+    }
+
+    @Test
+    public void testCanRemoveItemFromCart() throws IOException {
+        Cart cart = new Cart(new ArrayList<>());
+        Inventory inventory = new Inventory(createArrayListOfItems(collectItemsFromResources()));
+        Item itemToCart = inventory.getItems().get(0);
+        cart.add(itemToCart);
+        cart.remove(itemToCart);
+        Assertions.assertFalse(cart.getCartItems().contains(itemToCart));
+    }
 
 }
