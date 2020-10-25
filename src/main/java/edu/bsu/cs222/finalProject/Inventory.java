c30568c2a3c2daa66d971857e895f08869605a30
Caleb Van Lue <cpvanlue@bsu.edu>
2020-10-25 17:23:31 -0400

    Created method to create an ArrayList of Item objects from an ArrayList of strings.

--- src/main/java/edu/bsu/cs222/finalProject/Inventory.java
+++ src/main/java/edu/bsu/cs222/finalProject/Inventory.java
@@ -1,33 +1,18 @@
 package edu.bsu.cs222.finalProject;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Inventory {
 
-    public ArrayList<String[]> collectInventoryFromResources() throws IOException {
-        JsonParser parser = new JsonParser();
-        FileReader reader = new FileReader( "src/main/resources/items.json" );
+    private ArrayList<Item> items;
 
-        ArrayList<String[]> itemsList = new ArrayList<>();
-
-        JsonArray inventory = parser.parse(reader).getAsJsonArray();
-        reader.close();
-
-        for (JsonElement item : inventory)
-        {
-            String[] singleItem = new String[2];
-            singleItem[0] = item.getAsJsonObject().get("name").toString();
-            singleItem[1] = item.getAsJsonObject().get("price").getAsString();
-            itemsList.add(singleItem);
-        }
-        return itemsList;
+    public Inventory(ArrayList<Item> items) {
+        this.items = items;
     }
+
+
+
 
 }
 
--- src/main/java/edu/bsu/cs222/finalProject/Item.java
+++ src/main/java/edu/bsu/cs222/finalProject/Item.java
@@ -1,12 +1,48 @@
 package edu.bsu.cs222.finalProject;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Item {
 
     private String name;
-    private double price;
+    private String price;
 
-    public Item(String name, double price) {
+    public Item(String name, String price) {
         this.name = name;
         this.price = price;
+    }
+
+    public ArrayList<String[]> collectItemsFromResources() throws IOException {
+        JsonParser parser = new JsonParser();
+        FileReader reader = new FileReader( "src/main/resources/items.json" );
+
+        ArrayList<String[]> itemsList = new ArrayList<>();
+
+        JsonArray inventory = parser.parse(reader).getAsJsonArray();
+        reader.close();
+
+        for (JsonElement item : inventory)
+        {
+            String[] singleItem = new String[2];
+            singleItem[0] = item.getAsJsonObject().get("name").toString();
+            singleItem[1] = item.getAsJsonObject().get("price").getAsString();
+            itemsList.add(singleItem);
+        }
+        return itemsList;
+    }
+
+    public ArrayList<Item> createArrayListOfItems(ArrayList<String[]> itemData) {
+        ArrayList<Item> itemsList = new ArrayList<>();
+        for (int i = 0; i < itemData.size(); i++) {
+            Item newItem = new Item(itemData.get(i)[0], itemData.get(i)[1]);
+            itemsList.add(newItem);
+        }
+        return itemsList;
     }
 }
