002ef483eb54c125bc5e2d9005c0c6e2e5a89c9f
BrianJr03 <55070185+BrianJr03@users.noreply.github.com>
2020-10-25 20:33:39 -0400

    Added functionality to login button that launches the MainUI

--- src/main/java/edu/bsu/cs222/finalProject/LoginController.java
+++ src/main/java/edu/bsu/cs222/finalProject/LoginController.java
@@ -1,5 +1,28 @@
 package edu.bsu.cs222.finalProject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController
 {
+    public void launchMainUI( ActionEvent actionEvent )
+    {
+        try
+        {
+            Stage secondaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = loader.load(getClass().getResource( "/MainUI.fxml" ).openStream());
+            secondaryStage.setScene( new Scene( root, 725, 615 ) );
+            secondaryStage.setTitle("Grocery Shop");
+            secondaryStage.showAndWait();
+        }
+
+        catch ( IOException e )
+        { e.printStackTrace(); }
+    }
 }
