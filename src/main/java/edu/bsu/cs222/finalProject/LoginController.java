d05f5d4185708494b5b1db336bab275fa341378d
BrianJr03 <55070185+BrianJr03@users.noreply.github.com>
2020-10-25 20:45:34 -0400

    Configured LoginController to launch MainUI in same window instead of a new window

--- src/main/java/edu/bsu/cs222/finalProject/LoginController.java
+++ src/main/java/edu/bsu/cs222/finalProject/LoginController.java
@@ -1,28 +1,19 @@
 package edu.bsu.cs222.finalProject;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class LoginController
 {
-    public void launchMainUI( ActionEvent actionEvent )
-    {
-        try
-        {
-            Stage secondaryStage = new Stage();
-            FXMLLoader loader = new FXMLLoader();
-            Pane root = loader.load(getClass().getResource( "/MainUI.fxml" ).openStream());
-            secondaryStage.setScene( new Scene( root, 725, 615 ) );
-            secondaryStage.setTitle("Grocery Shop");
-            secondaryStage.showAndWait();
-        }
+    @FXML
+    private AnchorPane rootPane;
 
-        catch ( IOException e )
-        { e.printStackTrace(); }
+    public void launchMainUI( ActionEvent actionEvent ) throws IOException {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource( "/MainUI.fxml" ));
+            rootPane.getChildren().setAll( pane );
     }
 }
