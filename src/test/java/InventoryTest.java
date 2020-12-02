409b4442ad004dc99e2853b1377b191b70bf960a
Caleb Van Lue <cpvanlue@bsu.edu>
2020-12-02 00:31:28 -0500

    Cleans InventoryTest, removes unnecessary imports

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,18 +1,15 @@
 import edu.bsu.cs222.finalProject.Inventory;
-import edu.bsu.cs222.finalProject.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import static edu.bsu.cs222.finalProject.Inventory.collectItemsAsArrayList;
 
 public class InventoryTest {
 
-
     @Test
-    public void testCanReturnFirstItemFromKroger() throws IOException {
+    public void testCanReturnFirstItemFromStoreA() throws IOException {
         Inventory inventory = new Inventory(collectItemsAsArrayList("kroger"));
         Assertions.assertEquals("\"Bread\"", inventory.getItems().get(0).getName());
     }
@@ -30,27 +27,10 @@ public class InventoryTest {
     }
 
     @Test
-    public void testSearchForItem() throws IOException {
-        Inventory inventory =
-                new Inventory(collectItemsAsArrayList("walmart"));
-        ArrayList<String> actualOrder = new ArrayList <>();
-        ArrayList<String> expectedOrder = new ArrayList <>();
-
-        String nameOfItemToQuery = '"' + "Orange" + '"' ;
-        String priceOfItemToQuery = "1.69";
-        expectedOrder.add(nameOfItemToQuery);
-        expectedOrder.add(priceOfItemToQuery);
-
-        for (int i = 0; i < inventory.getItems().size(); i++) {
-            if (nameOfItemToQuery.equalsIgnoreCase(inventory.getItems().get( i ).getName())) {
-                System.out.printf("\nQueried item: %s\n", nameOfItemToQuery);
-                String name = inventory.getItems().get( i ).getName();
-                String price = inventory.getItems().get( i ).getPrice();
-                actualOrder.add( name ); actualOrder.add( price );
-                Item queriedItem = new Item(name, price);
-                System.out.printf("Found: %s\n", queriedItem.prettyPrintItem());
-            }
-        }
-        Assertions.assertEquals(expectedOrder, actualOrder);
+    public void canReturnFinalItemFromStoreA() throws IOException {
+        Inventory inventory = new Inventory(collectItemsAsArrayList("kroger"));
+        int inventorySize = inventory.getItems().size();
+        Assertions.assertEquals("\"Yogurt\"", inventory.getItems().get(inventorySize-1).getName());
     }
+
 }
