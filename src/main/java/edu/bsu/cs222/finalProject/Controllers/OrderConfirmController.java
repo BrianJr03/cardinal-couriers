a4b8d642b55c83c414c15730cce575ea2689a9d2
BrianJr03 <55070185+BrianJr03@users.noreply.github.com>
2020-12-08 02:20:30 -0500

    Adds 5 new items to each store inventory

--- src/main/java/edu/bsu/cs222/finalProject/Controllers/OrderConfirmController.java
+++ src/main/java/edu/bsu/cs222/finalProject/Controllers/OrderConfirmController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
