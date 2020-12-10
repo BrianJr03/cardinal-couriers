072c6a755bd684b44249496392687482f84225e9
Hailey Leonard <heleonard@bsu.edu>
2020-12-10 03:42:21 -0500

    Updates DeliveryMapTest to not pull data from the internet.

--- src/test/java/DeliveryMapTest.java
+++ src/test/java/DeliveryMapTest.java
@@ -1,30 +1,19 @@
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import edu.bsu.cs222.finalProject.DeliveryMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileReader;
 import java.io.IOException;
 
-import static edu.bsu.cs222.finalProject.DeliveryMap.collectJsonObjectFromGoogle;
-import static edu.bsu.cs222.finalProject.DeliveryMap.findDistanceFromBSU;
-
 public class DeliveryMapTest {
 
     @Test
-    public void testCanPullJSONDataFromGoogle() throws IOException {
+    public void isGoogleURLFormattedCorrectly() {
         String address = "1708 W Bethel Ave";
         String city = "Muncie";
         String state = "IN";
-        JsonObject results = collectJsonObjectFromGoogle(address,city,state);
-        FileReader reader = new FileReader("src/test/resources/Static JSON Data.json");
-        Assertions.assertEquals(JsonParser.parseReader(reader).getAsJsonObject(),results.getAsJsonObject());
-    }
-
-    @Test
-    public void canReturnProperDistanceDouble() throws IOException {
-        FileReader reader = new FileReader("src/test/resources/Static JSON Data.json");
-        JsonObject object = JsonParser.parseReader(reader).getAsJsonObject();
-        Assertions.assertEquals(1.7, findDistanceFromBSU(object));
+        Assertions.assertEquals("1708+W+Bethel+Ave+Muncie+IN",DeliveryMap.formatAddressForGoogle(address, city, state));
     }
 }
